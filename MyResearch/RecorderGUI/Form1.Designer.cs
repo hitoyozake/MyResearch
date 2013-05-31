1a91580e93ea9c99aae3803b4aeea57f6cdda094
hitoyozake <mikoto_1122@yahoo.co.jp>
2013-05-31 15:20:10 +0900

    proj追加

---
+++ MyResearch/RecorderGUI/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace RecorderGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(12, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(555, 179);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(555, 200);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 409);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ MyResearch/RecorderGUI/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace RecorderGUI
+{
+    public partial class Form1 : Form
+    {
+        bool debug_mode = false;
+        String recorder_name = @"./kinectrecorder.exe";
+        Process recorder = null;
+
+        enum Mode : int
+        {
+            E_STOP = 0,
+            E_RECORD = 1
+        };
+
+        Mode state = Mode.E_STOP;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void stop_recorder()
+        {
+
+        }
+
+        void prepare_recorder()
+        {
+            if (recorder != null)
+            {
+                //終了させる   
+            }
+            recorder = new Process();
+            recorder.StartInfo.UseShellExecute = false;
+            recorder.StartInfo.RedirectStandardInput = true;
+            recorder.StartInfo.RedirectStandardOutput = true;
+            recorder.StartInfo.FileName = recorder_name;
+
+            recorder.Start();
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Shiftを押しながらクリックするとデバッグモード
+            if ((Control.ModifierKeys & Keys.Shift) == Keys.Shift)
+            {
+                debug_mode = true;
+            }
+            else
+            {
+                debug_mode = false;
+            }
+
+            if (state == Mode.E_STOP)
+            {
+                state = Mode.E_RECORD;
+            }
+            else
+            {
+                state = Mode.E_STOP;
+            }
+        }
+    }
+}
